9967976d36f00001286e4cc3ee7147eeb3d81759
Michelle Mariani <mariani.public+github@gmail.com>
2026-03-14 22:47:03 -0400

    feat(global): Setup database library
    - Added RxDB and related dependencies to package.json
    - Created a database initialization function in src/storage/database.ts
    - Updated App.tsx to initialize the database and display its name

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import type { RxDatabase } from "rxdb";
+import Provider from "rxdb-hooks/dist/Provider";
+import {
+	type HabitDatabaseCollections,
+	initializeDatabase,
+} from "./storage/database";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	const [db, setDb] = useState<
+		RxDatabase<HabitDatabaseCollections> | undefined
+	>();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		let cancelled = false;
+
+		// RxDB instantiation can be asynchronous
+		const init = async () => {
+			const initializedDB = await initializeDatabase();
+			if (!cancelled && !db) {
+				setDb(initializedDB as RxDatabase<HabitDatabaseCollections>);
+			}
+		};
+
+		init();
+
+		return () => {
+			cancelled = true;
+		};
+	}, [db]);
+
+	return (
+		<Provider db={db}>
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</Provider>
+	);
 }
 
-export default App
+export default App;
---
+++ src/storage/database.ts
@@ -0,0 +1,41 @@
+import { addRxPlugin, createRxDatabase } from "rxdb/plugins/core";
+import { RxDBDevModePlugin } from "rxdb/plugins/dev-mode";
+import { getRxStorageLocalstorage } from "rxdb/plugins/storage-localstorage";
+import { wrappedValidateAjvStorage } from "rxdb/plugins/validate-ajv";
+import { v4 as uuid } from "uuid";
+import { type HabitCollection, habitSchema } from "./schemas/habit";
+
+addRxPlugin(RxDBDevModePlugin);
+
+export type HabitDatabaseCollections = {
+	habits: HabitCollection;
+};
+
+const initializeDatabase = async () => {
+	const storage = wrappedValidateAjvStorage({
+		storage: getRxStorageLocalstorage(),
+	});
+
+	const habitDatabase = await createRxDatabase<HabitDatabaseCollections>({
+		name: "habit-database",
+		storage: storage,
+		multiInstance: false,
+		closeDuplicates: true,
+	});
+
+	await habitDatabase.addCollections({
+		habits: {
+			schema: habitSchema,
+		},
+	});
+
+	const _newHabit = await habitDatabase.habits.insert({
+		id: uuid(),
+		title: "Go for a run",
+		description: "Run 5 kilometers every morning",
+	});
+
+	return habitDatabase;
+};
+
+export { initializeDatabase };
