be2bd60c664b239ca2d37fca4e9182e7fba3a409
Michelle Mariani <mariani.public+github@gmail.com>
2026-03-23 21:52:33 -0400

    style: :art: Improve linter for consistency - Change spacing to 2 spaces - Change single quotes to double quotes

--- src/storage/schemas/routine.ts
+++ src/storage/schemas/routine.ts
@@ -1,52 +1,52 @@
 import {
-	type ExtractDocumentTypeFromTypedRxJsonSchema,
-	type RxCollection,
-	type RxJsonSchema,
-	toTypedRxJsonSchema,
-} from "rxdb";
+  type ExtractDocumentTypeFromTypedRxJsonSchema,
+  type RxCollection,
+  type RxJsonSchema,
+  toTypedRxJsonSchema,
+} from 'rxdb';
 
 const routineSchemaLiteral = {
-	version: 0,
-	primaryKey: "id",
-	type: "object",
-	properties: {
-		id: {
-			type: "string",
-			final: true,
-			maxLength: 36,
-		},
-		title: {
-			type: "string",
-			maxLength: 255,
-		},
-		description: {
-			type: "string",
-		},
-		created: {
-			type: "string",
-			format: "date-time",
-			final: true,
-			//! Default to epoch time for testing only right now, as when the DB is created, the
-			//! hash will be different if we just use the current time and we will get DB6 error
-			default: new Date("1970-01-01T00:00:00.000Z").toISOString(),
-		},
-		habits: {
-			type: "array",
-			uniqueItems: true,
-			items: {
-				type: "string",
-				maxLength: 36,
-				minLength: 36,
-			},
-		},
-	},
-	required: ["id", "title", "habits"],
+  version: 0,
+  primaryKey: 'id',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      final: true,
+      maxLength: 36,
+    },
+    title: {
+      type: 'string',
+      maxLength: 255,
+    },
+    description: {
+      type: 'string',
+    },
+    created: {
+      type: 'string',
+      format: 'date-time',
+      final: true,
+      //! Default to epoch time for testing only right now, as when the DB is created, the
+      //! hash will be different if we just use the current time and we will get DB6 error
+      default: new Date('1970-01-01T00:00:00.000Z').toISOString(),
+    },
+    habits: {
+      type: 'array',
+      uniqueItems: true,
+      items: {
+        type: 'string',
+        maxLength: 36,
+        minLength: 36,
+      },
+    },
+  },
+  required: ['id', 'title', 'habits'],
 } as const;
 
 const schemaTyped = toTypedRxJsonSchema(routineSchemaLiteral);
 
 type RoutineDocType = ExtractDocumentTypeFromTypedRxJsonSchema<
-	typeof schemaTyped
+  typeof schemaTyped
 >;
 type RoutineCollection = RxCollection<RoutineDocType>;
 const routineSchema: RxJsonSchema<RoutineDocType> = routineSchemaLiteral;
