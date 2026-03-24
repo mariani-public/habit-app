6d042d83b511fc46b1fe7f51ccbcc39dc5082b29
Michelle Mariani <mariani.public+github@gmail.com>
2026-03-23 21:55:48 -0400

    refactor(tracking-grid): :lipstick: Adjust tracking grid to use mantine components - Remove custom CSS and HTML and replace with Mantine's Grid and ThemeIcon components for better styling and consistency. - Add IconCheck from @tabler/icons-react to display checkmarks in the tracking grid. - Add postcss boilerplate for mantine - Remove react-aria and lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,45 @@
-import { useEffect, useState } from "react";
-import type { RxDatabase } from "rxdb";
-import Provider from "rxdb-hooks/dist/Provider";
-import { TrackingGrid } from "./pages/TrackingGrid/TrackingGrid";
+import { useEffect, useState } from 'react';
+import type { RxDatabase } from 'rxdb';
+import DatabaseProvider from 'rxdb-hooks/dist/Provider';
+import { TrackingGrid } from './pages/TrackingGrid/TrackingGrid';
 import {
-	type HabitDatabaseCollections,
-	initializeDatabase,
-} from "./storage/database";
+  type HabitDatabaseCollections,
+  initializeDatabase,
+} from './storage/database';
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
 
 function App() {
-	const [db, setDb] = useState<
-		RxDatabase<HabitDatabaseCollections> | undefined
-	>();
+  const [db, setDb] = useState<
+    RxDatabase<HabitDatabaseCollections> | undefined
+  >();
 
-	useEffect(() => {
-		let cancelled = false;
+  useEffect(() => {
+    let cancelled = false;
 
-		// RxDB instantiation can be asynchronous
-		const init = async () => {
-			const initializedDB = await initializeDatabase();
-			if (!cancelled && !db) {
-				setDb(initializedDB as RxDatabase<HabitDatabaseCollections>);
-			}
-		};
+    // RxDB instantiation can be asynchronous
+    const init = async () => {
+      const initializedDB = await initializeDatabase();
+      if (!cancelled && !db) {
+        setDb(initializedDB as RxDatabase<HabitDatabaseCollections>);
+      }
+    };
 
-		init();
+    init();
 
-		return () => {
-			cancelled = true;
-		};
-	}, [db]);
+    return () => {
+      cancelled = true;
+    };
+  }, [db]);
 
-	return (
-		<Provider db={db}>
-			<TrackingGrid />
-		</Provider>
-	);
+  return (
+    <DatabaseProvider db={db}>
+      <MantineProvider>
+        <TrackingGrid />
+      </MantineProvider>
+    </DatabaseProvider>
+  );
 }
 
 export default App;
--- src/pages/TrackingGrid/TrackingGrid.tsx
+++ src/pages/TrackingGrid/TrackingGrid.tsx
@@ -1,59 +1,65 @@
-import { useEffect, useState } from "react";
-import { useRxCollection } from "rxdb-hooks";
+import { Grid, ThemeIcon } from '@mantine/core';
+import { IconCheck } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import { useRxCollection } from 'rxdb-hooks';
 import type {
-	HabitCollection,
-	HabitDocType,
-} from "../../storage/schemas/habit";
-import styles from "./TrackingGrid.module.css";
+  HabitCollection,
+  HabitDocType,
+} from '../../storage/schemas/habit';
 
 const DAYS = {
-	0: "Sunday",
-	1: "Monday",
-	2: "Tuesday",
-	3: "Wednesday",
-	4: "Thursday",
-	5: "Friday",
-	6: "Saturday",
+  0: 'Sunday',
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
 };
 
 const TrackingGrid = () => {
-	const habitsCollection = useRxCollection("habits") as HabitCollection;
-	const [habits, setHabits] = useState<HabitDocType[]>([]);
+  const habitsCollection = useRxCollection('habits') as HabitCollection;
+  const [habits, setHabits] = useState<HabitDocType[]>([]);
 
-	useEffect(() => {
-		const fetchHabits = async () => {
-			if (habitsCollection) {
-				const habits = await habitsCollection.find().exec();
-				setHabits(habits);
-			}
-		};
+  useEffect(() => {
+    const fetchHabits = async () => {
+      if (habitsCollection) {
+        const habits = await habitsCollection.find().exec();
+        setHabits(habits);
+      }
+    };
 
-		fetchHabits();
-	}, [habitsCollection]);
+    fetchHabits();
+  }, [habitsCollection]);
 
-	console.log("habits", habits);
+  console.log('habits', habits);
 
-	return (
-		<div className={styles["tracking-grid"]}>
-			<div>Habit</div>
-			{Object.entries(DAYS).map(([dayNum, dayName]) => (
-				<div key={dayNum}>
-					<h3>{dayName}</h3>
-				</div>
-			))}
-			{habits.map((habit) => (
-				<>
-					<div key={habit.id}>{habit.title}</div>
-					{Object.entries(DAYS).map(([dayNum]) => (
-						<div
-							key={dayNum}
-							style={{ border: "2px solid orange", borderRadius: "50%" }}
-						/>
-					))}
-				</>
-			))}
-		</div>
-	);
+  return (
+    <Grid columns={Object.keys(DAYS).length + 1}>
+      <Grid.Col span={1}>
+        <div>Habit</div>
+      </Grid.Col>
+      {Object.entries(DAYS).map(([dayNum, dayName]) => (
+        <Grid.Col span={1} key={dayNum}>
+          <h3>{dayName}</h3>
+        </Grid.Col>
+      ))}
+      {habits.map((habit) => (
+        <>
+          <Grid.Col span={1} key={habit.id}>
+            <div>{habit.title}</div>
+          </Grid.Col>
+          {Object.entries(DAYS).map(([dayNum]) => (
+            <Grid.Col span={1} key={dayNum}>
+              <ThemeIcon radius="lg">
+                <IconCheck height="1rem" />
+              </ThemeIcon>
+            </Grid.Col>
+          ))}
+        </>
+      ))}
+    </Grid>
+  );
 };
 
 export { TrackingGrid };
