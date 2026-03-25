529dffbe0be84e2167513af71d83f7c55a39b811
Michelle Mariani <mariani.public+github@gmail.com>
2026-03-24 21:44:32 -0400

    feat: :sparkles: Add ability to create a habit - Add notifications provider - Add ability to add a new habit

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,19 @@ import {
   initializeDatabase,
 } from './storage/database';
 import '@mantine/core/styles.css';
-
+import '@mantine/notifications/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { NewHabitFormContainer } from './pages/NewHabitForm/NewHabitFormContainer';
 
 function App() {
   const [db, setDb] = useState<
     RxDatabase<HabitDatabaseCollections> | undefined
   >();
+  const [isShowingNewHabitForm, setIsShowingNewHabitForm] = useState(false);
+
+  const showNewHabitForm = () =>
+    setIsShowingNewHabitForm(!isShowingNewHabitForm);
 
   useEffect(() => {
     let cancelled = false;
@@ -21,7 +27,7 @@ function App() {
     // RxDB instantiation can be asynchronous
     const init = async () => {
       const initializedDB = await initializeDatabase();
-      if (!cancelled && !db) {
+      if (!cancelled) {
         setDb(initializedDB as RxDatabase<HabitDatabaseCollections>);
       }
     };
@@ -31,12 +37,14 @@ function App() {
     return () => {
       cancelled = true;
     };
-  }, [db]);
+  }, []);
 
   return (
     <DatabaseProvider db={db}>
       <MantineProvider>
-        <TrackingGrid />
+        <Notifications position="top-center" />
+        <button onClick={showNewHabitForm}>Add habit</button>
+        {!isShowingNewHabitForm ? <TrackingGrid /> : <NewHabitFormContainer />}
       </MantineProvider>
     </DatabaseProvider>
   );
--- src/storage/database.ts
+++ src/storage/database.ts
@@ -2,7 +2,6 @@ import { addRxPlugin, createRxDatabase } from 'rxdb/plugins/core';
 import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
 import { getRxStorageLocalstorage } from 'rxdb/plugins/storage-localstorage';
 import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv';
-import { v4 as uuid } from 'uuid';
 import { type HabitCollection, habitSchema } from './schemas/habit';
 
 addRxPlugin(RxDBDevModePlugin);
@@ -27,12 +26,6 @@ const initializeDatabase = async () => {
     habits: {
       schema: habitSchema,
     },
-  });
-
-  const _newHabit = await habitDatabase.habits.insert({
-    id: uuid(),
-    title: 'Go for a run',
-    description: 'Run 5 kilometers every morning',
   });
 
   return habitDatabase;
